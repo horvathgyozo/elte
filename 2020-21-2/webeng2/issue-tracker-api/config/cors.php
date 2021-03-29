8d969d25b684a468370cfca02b748d675daffd45
Győző Horváth <gyozo.horvath@inf.elte.hu>
2021-03-29 14:08:07 +0200

    webeng2/fortify missing pieces

--- 2020-21-2/webeng2/issue-tracker-api/config/cors.php
+++ 2020-21-2/webeng2/issue-tracker-api/config/cors.php
@@ -15,7 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*', 
+        'sanctum/csrf-cookie',
+        'login',
+        'logout',
+        'register',
+    ],
 
     'allowed_methods' => ['*'],
 
