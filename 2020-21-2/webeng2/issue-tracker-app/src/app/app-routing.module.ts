7271b313d3fa87af8245854a19cd665771967c1b
Győző Horváth <gyozo.horvath@inf.elte.hu>
2021-04-19 13:48:59 +0200

    webeng2/9. practice

--- 2020-21-2/webeng2/issue-tracker-app/src/app/app-routing.module.ts
+++ 2020-21-2/webeng2/issue-tracker-app/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IssueDetailComponent } from './issue-detail/issue-detail.component';
+import { IssueFormComponent } from './issue-form/issue-form.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'issues',
+    component: IssueListComponent,
+  },
+  {
+    path: 'issues/new',
+    component: IssueFormComponent,
+  },
+  {
+    path: 'issues/:id',
+    component: IssueDetailComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
