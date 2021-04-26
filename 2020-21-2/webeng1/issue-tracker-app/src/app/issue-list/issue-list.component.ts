632e9162418d3dff995ad560402e8f8c5245edbf
Győző Horváth <gyozo.horvath@inf.elte.hu>
2021-04-26 11:34:16 +0200

    webeng1/10. practice

--- 2020-21-2/webeng1/issue-tracker-app/src/app/issue-list/issue-list.component.ts
+++ 2020-21-2/webeng1/issue-tracker-app/src/app/issue-list/issue-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from '../issue';
 
 @Component({
   selector: 'app-issue-list',
@@ -6,14 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue-list.component.css'],
 })
 export class IssueListComponent implements OnInit {
-  issues = [
+  issues: Issue[] = [
     { id: 1, title: 'issue1', description: 'desc1', place: 'place1', status: 'NEW' },
     { id: 2, title: 'issue2', description: 'desc2', place: 'place2', status: 'DOING' },
     { id: 3, title: 'issue3', description: 'desc3', place: 'place3', status: 'DOING' },
     { id: 4, title: 'issue4', description: 'desc4', place: 'place4', status: 'DONE' },
   ];
+  status = 'ALL';
+  filteredIssues = this.issues;
+  selectedIssue: Issue = null;
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.filterIssues();
+  }
+
+  filterIssues(): void {
+    this.filteredIssues =
+      this.status === 'ALL' ? this.issues : this.issues.filter((issue) => issue.status === this.status);
+  }
+
+  handleStatusChange(newStatus: string): void {
+    this.status = newStatus;
+    this.filterIssues();
+  }
+
+  handleSave(issue): void {
+    Object.assign(this.selectedIssue, issue);
+    this.selectedIssue = null;
+  }
 }
