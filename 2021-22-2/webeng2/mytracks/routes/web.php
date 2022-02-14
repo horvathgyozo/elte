ee8b115419d38accb643aa05b79883b79c6a1d1d
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-02-14 13:54:00 +0100

    2. practice

---
+++ 2021-22-2/webeng2/mytracks/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $numberOfProjects = 20;
+    return view('main', [
+        'numberOfProjects' => $numberOfProjects,
+    ]); // main.blade.php
+});
+// Route::get('/projects', function () {
+//     return view('projects.list'); // projects/list.blade.php
+// });
+Route::get('/projects', [ProjectController::class, 'index']);
