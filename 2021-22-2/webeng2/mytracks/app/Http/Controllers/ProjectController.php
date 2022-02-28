ed0449c07ae659935c11910a23fae7648d1f91f3
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-02-28 15:12:25 +0100

    4. practice

--- 2021-22-2/webeng2/mytracks/app/Http/Controllers/ProjectController.php
+++ 2021-22-2/webeng2/mytracks/app/Http/Controllers/ProjectController.php
@@ -2,28 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index() {
-        $projects = [
-            [
-                'id' => 1,
-                'name' => 'Title1',
-                'description' => 'Description1',
-                'bg_url' => null,
-            ],
-            [
-                'id' => 2,
-                'name' => 'Title2',
-                'description' => 'Description2',
-                'bg_url' => 'http://clipart-library.com/images/di9KAoMjT.jpg',
-            ],
-        ];
+        $projects = Project::all();
         return view('projects.list', [
             'projects' => $projects,
         ]); // projects/list.blade.php
+    }
+
+    public function show($id) {
+        $project = Project::findOrFail($id);
+        return view('projects.show', [
+            'project' => $project,
+        ]); // projects/show.blade.php
     }
 
     public function create() {
@@ -37,30 +32,32 @@ class ProjectController extends Controller
             'image_url'     => 'nullable|url',
         ]);
         // dd($validated_data);
-        // Database insert
+        Project::create($validated_data);
         return redirect('/projects'); // GET
     }
 
-    public function edit() {
-        $project = [
-            'id' => 1,
-            'name' => 'Title1',
-            'description' => 'Description1',
-            'image_url' => null,
-        ];
+    public function edit($id) {
+        $project = Project::findOrFail($id);
         return view('projects.edit', [
             'project' => $project,
         ]); // projects/edit.blade.php
     }
 
-    public function update(Request $request) {
+    public function update($id, Request $request) {
         $validated_data = $request->validate([
             'name'  => 'required',
             'description'   => 'nullable',
             'image_url'     => 'nullable|url',
         ]);
         // dd($validated_data);
-        // Database update
+        $project = Project::findOrFail($id);
+        $project->update($validated_data);
         return redirect('/projects'); // GET
+    }
+
+    public function delete($id) {
+        $project = Project::findOrFail($id);
+        $project->delete();
+        return redirect('/projects');
     }
 }
