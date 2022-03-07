ddd01c92815a0681bb7d6163c310887df5d1ca5c
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-03-07 13:49:34 +0100

    5. practice

---
+++ 2021-22-2/webeng2/mytracks/app/Http/Controllers/TrackController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Track;
+use App\Http\Requests\StoreTrackRequest;
+use App\Http\Requests\UpdateTrackRequest;
+use App\Models\Project;
+
+class TrackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Project $project)
+    {
+        return view('tracks.create', [
+            'project' => $project,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTrackRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Project $project, StoreTrackRequest $request)
+    {
+        $project->tracks()->create( $request->validated() );
+        return redirect()->route('projects.show', ['project' => $project->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Track $track)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Track $track)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTrackRequest  $request
+     * @param  \App\Models\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTrackRequest $request, Track $track)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Track  $track
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Track $track)
+    {
+        //
+    }
+}
