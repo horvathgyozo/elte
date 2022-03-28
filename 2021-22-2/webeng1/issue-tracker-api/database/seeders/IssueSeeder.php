db9a5e9229e127a6b7274b6d7cd918a93eb18f3a
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-03-28 12:15:39 +0200

    7. practice

--- 2021-22-2/webeng1/issue-tracker-api/database/seeders/IssueSeeder.php
+++ 2021-22-2/webeng1/issue-tracker-api/database/seeders/IssueSeeder.php
@@ -2,19 +2,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\Issue;
+use Database\Factories\IssueFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class IssueSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(IssueSeeder::class);
+        Issue::factory(5)->create();
     }
 }
