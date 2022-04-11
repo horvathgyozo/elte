dd6220079c85e24d1db66bc77a83a1b7641166f8
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-04-11 11:41:59 +0200

    9. practice: clickale static version

--- 2021-22-2/webeng1/issue-tracker-app/src/app/app.module.ts
+++ 2021-22-2/webeng1/issue-tracker-app/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainPageComponent } from './main-page/main-page.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueDetailComponent } from './issue-detail/issue-detail.component';
+import { IssueFormComponent } from './issue-form/issue-form.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    IssueListComponent,
+    IssueDetailComponent,
+    IssueFormComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
