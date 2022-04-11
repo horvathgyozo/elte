e7f5e8ef09c20df0e8cd35ee101b568d4d11db27
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-04-11 13:49:42 +0200

    9. practice: clickable static version

--- 2021-22-2/webeng2/issue-tracker-app/src/app/app.module.ts
+++ 2021-22-2/webeng2/issue-tracker-app/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { IssueListComponent } from './issue-list/issue-list.component';
+import { IssueDetailComponent } from './issue-detail/issue-detail.component';
+import { IssueFormComponent } from './issue-form/issue-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainPageComponent,
+    IssueListComponent,
+    IssueDetailComponent,
+    IssueFormComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ 2021-22-2/webeng2/issue-tracker-app/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'],
+})
+export class NavBarComponent implements OnInit {
+  public isMenuCollapsed = true;
+  constructor() {}
+
+  ngOnInit(): void {}
+}
