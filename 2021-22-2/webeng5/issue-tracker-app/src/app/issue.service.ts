4b3383a7075b728cf8ed71ab6e4262d36b0873ab
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2022-05-11 09:56:17 +0200

    12. practice

--- 2021-22-2/webeng5/issue-tracker-app/src/app/issue.service.ts
+++ 2021-22-2/webeng5/issue-tracker-app/src/app/issue.service.ts
@@ -1,5 +1,14 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 import { Issue } from './issue';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
@@ -11,31 +20,27 @@ export class IssueService {
     { id: 3, title: 'issue3', description: 'desc3', place: 'place3', status: 'DONE' },
     { id: 4, title: 'issue4', description: 'desc4', place: 'place4', status: 'NEW' },
   ];
+  private issuesUrl = 'http://localhost:8000/api/issues';
 
-  getIssues() {
-    return this.issues;
+  constructor(private http: HttpClient) {}
+
+  async getIssues() {
+    return await lastValueFrom(this.http.get<Issue[]>(this.issuesUrl));
   }
 
-  getIssue(id: number) {
-    return this.issues.find((issue) => issue.id === id);
+  async getIssue(id: number) {
+    return await lastValueFrom(this.http.get<Issue>(`${this.issuesUrl}/${id}`));
   }
 
-  updateIssue(id: number, modifiedIssue: Issue) {
-    const issue = this.getIssue(id);
-    Object.assign(issue, modifiedIssue);
-    return issue;
+  async updateIssue(id: number, modifiedIssue: Issue) {
+    return await lastValueFrom(this.http.patch<Issue>(`${this.issuesUrl}/${id}`, modifiedIssue, httpOptions));
   }
 
-  addIssue(newIssue: Issue) {
-    const id = this.issues.length + 1;
-    newIssue.id = id;
-    this.issues.push(newIssue);
-    return newIssue;
+  async addIssue(newIssue: Issue) {
+    return await lastValueFrom(this.http.post<Issue>(this.issuesUrl, newIssue, httpOptions));
   }
 
-  deleteIssue(id: number) {
-    const pos = this.issues.findIndex((issue) => issue.id === id);
-    this.issues.splice(pos, 1); // deleting
-    // this.issues = this.issues.filter((issue) => issue.id !== id);
+  async deleteIssue(id: number) {
+    return await lastValueFrom(this.http.delete<null>(`${this.issuesUrl}/${id}`, httpOptions));
   }
 }
