0da8af43269ea4bfc56dbd2039839b704060b192
Gyozo Horvath <gyozo.horvath@inf.elte.hu>
2023-03-13 11:49:44 +0100

    3. practice

--- 2022-23-2/webeng/mytracks/app/Http/Controllers/ProjectController.php
+++ 2022-23-2/webeng/mytracks/app/Http/Controllers/ProjectController.php
@@ -9,16 +9,54 @@ class ProjectController extends Controller
     public function index() {
         $projects = [
             [
-                "title" => "project1",
+                "id" => "1",
+                "name" => "project1",
                 "description" => "desc1",
             ],
             [
-                "title" => "project2",
+                "id" => "2",
+                "name" => "project2",
                 "description" => "desc2",
             ],
         ];
         return view('projects.list', [
             "projects" => $projects,
         ]); //projects/list.blade.php
+    }
+
+    public function create() {
+        return view('projects.create'); //projects/create.blade.php
+    }
+    public function store(Request $request) {
+        $validated_data = $request->validate([
+            'name'          => 'required',
+            'description'   => 'nullable',
+            'image_url'     => 'nullable|url',
+        ]);
+        // dd($validated_data);
+        // save the validated data
+        return redirect("/projects");
+    }
+    public function edit() {
+        // read the project from the database
+        $project = [
+            "id" => "1",
+            "name" => "project1",
+            "description" => "desc1",
+            "image_url" => "",
+        ];
+        return view('projects.edit', [
+            "project" => $project,
+        ]); //projects/edit.blade.php
+    }
+    public function update(Request $request) {
+        $validated_data = $request->validate([
+            'name'          => 'required',
+            'description'   => 'nullable',
+            'image_url'     => 'nullable|url',
+        ]);
+        // dd($validated_data);
+        // modify the validated data in the database
+        return redirect("/projects");
     }
 }
---
+++ 2022-23-2/webeng/mytracks/resources/views/projects/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.main')
+
+@section('title', 'New project page')
+
+@section('content')
+  <div class="container py-3">
+    <h2>Edit a project</h2>
+    <form action="/projects/{{ $project["id"] }}" method="post">
+      @csrf
+      @method('PUT')
+
+      <div class="form-group">
+        <label for="name">Project name</label>
+        <input type="text" 
+              class="form-control @error('name') is-invalid @enderror" 
+              name="name" 
+              placeholder=""
+              value="{{ old('name', $project["name"]) }}"
+        >
+        @error('name')
+          <div class="invalid-feedback">
+            Please choose a username.
+          </div>
+        @enderror
+      </div>
+      
+      <div class="form-group">
+        <label for="description">Description</label>
+        <textarea class="form-control @error('description') is-invalid @enderror" name="description" rows="3">{{ old('description', $project["description"]) }}</textarea>
+        @error('description')
+          <div class="invalid-feedback">
+            Please choose a username.
+          </div>
+        @enderror
+      </div>
+      
+      <div class="form-group">
+        <label for="image_url">Background image URL</label>
+        <input type="text" class="form-control @error('image_url') is-invalid @enderror" name="image_url" placeholder=""
+          value="{{ old('image_url', $project["image_url"]) }}"
+        >
+        @error('image_url')
+          <div class="invalid-feedback">
+            Please choose a username.
+          </div>
+        @enderror
+      </div>
+
+      <div class="form-group">
+        <button type="submit" class="btn btn-primary">Add new project</button>
+      </div>
+
+    </form>
+  </div>
+@endsection
