f59807bff8b20119549062a447223378f91a853d
Horváth Győző <horvath.gyozo@inf.elte.hu>
2026-03-17 11:23:45 +0100

    awp1/installing laravel breeze

--- 2025-26-2/awp1/recipebox/routes/web.php
+++ 2025-26-2/awp1/recipebox/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,3 +22,19 @@ use Illuminate\Support\Facades\Route;
 // Route::delete('/recipes/{recipe}', [RecipeController::class, 'delete'])->name("recipes.delete");
 
 Route::resource('/recipes', RecipeController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
